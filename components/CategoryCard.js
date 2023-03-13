27e47af5fb4a4c90e9980b546b3e7b8e1c8a74de
Hocine <mhocine1994@gmail.com>
2023-03-13 19:21:40 +0100

    tst

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 const CategoryCard = ({imgUrl, title}) => {
   return (
     <TouchableOpacity className='relative mr-2'>
-      <Image 
+      <Image  
         source={{uri:imgUrl}}
         className='h-20 w-20 rounded'
       />
